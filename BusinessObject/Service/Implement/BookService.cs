d07b72a125492a82f633e1c81522bb290b8a4034
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-12-12 09:28:42 +0700

    implemented book list

--- BusinessObject/Service/Implement/BookService.cs
+++ BusinessObject/Service/Implement/BookService.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.DataAccess;
+﻿using BusinessObject.Model;
+using DataAccess.DataAccess;
 using DataAccess.Generic.UnitOfWork;
 
 namespace BusinessObject.Service.Implement {
@@ -11,7 +12,129 @@ namespace BusinessObject.Service.Implement {
 
         public Task<IEnumerable<Book>> GetAll() {
             IEnumerable<Book> books = _unitOfWork.BookRepository.GetAll();
-            return Task.FromResult(books);
+            IEnumerable<Category> categories = _unitOfWork.CategoryRepository.GetAll();
+
+            IEnumerable<Book> data = from book in books
+                join category in categories
+                    on book.CategoryId equals category.CategoryId
+                select new Book {
+                    BookId = book.BookId,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Isbn = book.Isbn,
+                    Price = book.Price,
+                    ImageUrl = book.ImageUrl,
+                    Status = book.Status,
+                    StockQuantity = book.StockQuantity,
+                    Description = book.Description,
+                    CategoryId = book.CategoryId,
+                    Category = category
+                };
+
+            return Task.FromResult(data);
+        }
+
+        public Task<IEnumerable<Book>> GetBookList(GetBooksDto getBooksDto) {
+            IEnumerable<Book> books = _unitOfWork.BookRepository.GetAll();
+            IEnumerable<Category> categories = _unitOfWork.CategoryRepository.GetAll();
+
+            IEnumerable<Book> query = from book in books
+                join category in categories
+                    on book.CategoryId equals category.CategoryId
+                select new Book {
+                    BookId = book.BookId,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Isbn = book.Isbn,
+                    Price = book.Price,
+                    ImageUrl = book.ImageUrl,
+                    Status = book.Status,
+                    StockQuantity = book.StockQuantity,
+                    Description = book.Description,
+                    CategoryId = book.CategoryId,
+                    Category = category
+                };
+
+            if (!string.IsNullOrEmpty(getBooksDto.Title)) {
+                query = query.Where(book => book.Title.Contains(getBooksDto.Title));
+            }
+
+            if (!string.IsNullOrEmpty(getBooksDto.Category)) {
+                query = query.Where(book => book.Category.Name.Contains(getBooksDto.Category));
+            }
+
+            if (getBooksDto.Sort is { Field: not null, Direction: not null }) {
+                string sortField = getBooksDto.Sort.Field;
+                string sortDirection = getBooksDto.Sort.Direction;
+
+                switch (sortField) {
+                    case SortField.Title:
+                        query = sortDirection == SortDirection.Asc
+                            ? query.OrderBy(book => book.Title)
+                            : query.OrderByDescending(book => book.Title);
+                        break;
+                    case SortField.Category:
+                        query = sortDirection == SortDirection.Asc
+                            ? query.OrderBy(book => book.Category.Name)
+                            : query.OrderByDescending(book => book.Category.Name);
+                        break;
+                    case SortField.Price:
+                        query = sortDirection == SortDirection.Asc
+                            ? query.OrderBy(book => book.Price)
+                            : query.OrderByDescending(book => book.Price);
+                        break;
+                    case SortField.Status:
+                        query = sortDirection == SortDirection.Asc
+                            ? query.OrderBy(book => book.Status)
+                            : query.OrderByDescending(book => book.Status);
+                        break;
+                    case SortField.Stock:
+                        query = sortDirection == SortDirection.Asc
+                            ? query.OrderBy(book => book.StockQuantity)
+                            : query.OrderByDescending(book => book.StockQuantity);
+                        break;
+                }
+            }
+
+            int currentPage = getBooksDto.CurrentPage <= 0 ? 1 : getBooksDto.CurrentPage;
+            int pageSize = getBooksDto.PageSize <= 0 ? 10 : getBooksDto.PageSize;
+            query = query.Skip((currentPage - 1) * pageSize).Take(pageSize);
+
+            return Task.FromResult(query);
+        }
+
+        public Task<int> CountBookList(GetBooksDto getBooksDto) {
+            IEnumerable<Book> books = _unitOfWork.BookRepository.GetAll();
+            IEnumerable<Category> categories = _unitOfWork.CategoryRepository.GetAll();
+
+            IEnumerable<Book> query = from book in books
+                join category in categories
+                    on book.CategoryId equals category.CategoryId
+                select new Book {
+                    BookId = book.BookId,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Isbn = book.Isbn,
+                    Price = book.Price,
+                    ImageUrl = book.ImageUrl,
+                    Status = book.Status,
+                    StockQuantity = book.StockQuantity,
+                    Description = book.Description,
+                    CategoryId = book.CategoryId,
+                    Category = category
+                };
+
+            if (!string.IsNullOrEmpty(getBooksDto.Title)) {
+                query = query.Where(book => book.Title.Contains(getBooksDto.Title));
+            }
+
+            if (!string.IsNullOrEmpty(getBooksDto.Category)) {
+                query = query.Where(book => book.Category.Name.Contains(getBooksDto.Category));
+            }
+
+            return Task.FromResult(query.Count());
+        }
+        
         }
     }
 }
--- Presentation/Pages/Book/Index.cshtml.cs
+++ Presentation/Pages/Book/Index.cshtml.cs
@@ -1,4 +1,6 @@
+using BusinessObject.Model;
 using BusinessObject.Service;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Presentation.Pages.Book {
@@ -9,12 +11,38 @@ namespace Presentation.Pages.Book {
             _bookService = bookService;
         }
 
+        [BindProperty(SupportsGet = true)] public string Title { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)] public string Category { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)] public int CurrentPage { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)] public int PageSize { get; set; } = 10;
+
+        [BindProperty(SupportsGet = true)] public string SortBy { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)] public string SortOrder { get; set; } = default!;
+
+        public int TotalPages { get; set; }
+
         public IList<DataAccess.DataAccess.Book> Book { get; set; } = default!;
 
         public async Task OnGetAsync() {
-            IEnumerable<DataAccess.DataAccess.Book>? books = await _bookService.GetAll();
-            if (books != null) {
-                Book = books.ToList();
+            // IEnumerable<DataAccess.DataAccess.Book>? books = await _bookService.GetAll();
+            GetBooksDto getBooksDto = new() {
+                Title = Title,
+                Category = Category,
+                CurrentPage = CurrentPage,
+                PageSize = PageSize,
+                Sort = new SortBookDto { Field = SortBy, Direction = SortOrder }
+            };
+
+            IEnumerable<DataAccess.DataAccess.Book>? data = await _bookService.GetBookList(getBooksDto);
+            List<DataAccess.DataAccess.Book>? books = data.ToList();
+            int count = await _bookService.CountBookList(getBooksDto);
+            if (books != null && books.Any()) {
+                Book = books;
+                TotalPages = (int)Math.Ceiling(decimal.Divide(count, PageSize));
             }
         }
     }
