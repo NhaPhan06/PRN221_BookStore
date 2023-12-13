90f50cfa42c8d63b5a69f6a4f9ad7cbff87840c1
Thong <thongpmse161425@fpt.edu.vn>
2023-12-13 23:59:20 +0700

    Update status vs list

--- BusinessLayer/Service/IOrderService.cs
+++ BusinessLayer/Service/IOrderService.cs
@@ -1,4 +1,14 @@
-﻿namespace BusinessLayer.Service {
+﻿using DataAccess.DataAccess;
+
+namespace BusinessLayer.Service {
     public interface IOrderService {
+        List<Order> GetAll();
+        List<Order> Search();
+        Order DisableOrder(Guid id);
+        Order GetOrderById(Guid id);
+        Order ReciveOrder(Guid id);
+        Order DeliveryOrder(Guid id);
+        Order ConfirmOrder(Guid id);
+
     }
 }
