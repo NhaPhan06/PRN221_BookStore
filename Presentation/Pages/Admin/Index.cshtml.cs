23b1845217bb2f8d9e21d8a1c80b7653200aa53d
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-12-14 16:04:19 +0700

    add admin template

---
+++ Presentation/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.Pages.Admin {
+    public class Index : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
