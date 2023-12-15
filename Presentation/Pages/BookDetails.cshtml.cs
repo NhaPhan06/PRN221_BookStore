f01442b4b552f054524298fbb424f2b1c1348e64
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-12-15 16:18:43 +0700

    update code - add to cart in detail doesn't work

--- Presentation/Pages/BookDetails.cshtml.cs
+++ Presentation/Pages/BookDetails.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Service;
+using DataAccess.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Presentation.Pages {
     public class BookDetails : PageModel {
@@ -21,5 +23,55 @@ namespace Presentation.Pages {
 
             Book = result;
         }
+         public async Task<IActionResult> OnPostAddToCart(Guid Id) {
+                    bool check = false;
+                    List<Carts> cartsList = new();
+                    Carts newCart = new();
+                    DataAccess.DataAccess.Book book = await _bookService.GetBookById(Id);
+        
+                    // get cart from session
+                    string? json = HttpContext.Session.GetString("cart");
+        
+                    // deserialize cart
+                    if (json != null) {
+                        cartsList = JsonConvert.DeserializeObject<List<Carts>>(json);
+                    }
+        
+                    // add book to cart
+                    if (cartsList.Count != 0) {
+                        foreach (Carts c in cartsList) {
+                            if (c.BookId == book.BookId) {
+                                c.StockQuantity += 1;
+                                check = true;
+                                break;
+                            }
+                        }
+        
+                        if (check == false) {
+                            newCart.BookId = book.BookId;
+                            newCart.StockQuantity = 1;
+                            newCart.Title = book.Title;
+                            newCart.Price = book.Price;
+                            newCart.ImageUrl = book.ImageUrl;
+                            cartsList.Add(newCart);
+                        }
+                    } else {
+                        newCart.BookId = book.BookId;
+                        newCart.StockQuantity = 1;
+                        newCart.Title = book.Title;
+                        newCart.Price = book.Price;
+                        newCart.ImageUrl = book.ImageUrl;
+                        cartsList.Add(newCart);
+                    }
+        
+                    //Remove old Session
+                    HttpContext.Session.Remove("cart");
+        
+                    // serialize cart
+                    json = JsonConvert.SerializeObject(cartsList);
+                    HttpContext.Session.SetString("cart", json);
+        
+                    return Redirect("shop");
+                }
     }
 }
